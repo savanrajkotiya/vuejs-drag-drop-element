0949dfa413ae7590241d31dad0a6e87954102ec1
Savan Rajkotiya <savan.r@yudiz.in>
2020-04-14 19:01:28 +0530

    initial commit

---
+++ src/components/DragDrop.vue
@@ -0,0 +1,184 @@
+<template>
+  <div id="shopping-list">
+    <h1>{{ header}}</h1>
+    <div style="width:300px;height:300px;border:2px solid;" ref="bounded">
+      <div v-draggable="draggableValue">Drag Me!</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Draggable } from "draggable-vue-directive";
+export default {
+  name: "DragDrop",
+  directives: {
+    Draggable
+  },
+  data() {
+    return {
+      header: "VUE Drag Area",
+      draggableValue: {
+        onDragEnd: this.onPosChanged,
+        boundingElement: ""
+      }
+    };
+  },
+  methods: {
+    onPosChanged(positionDiff, absolutePosition, event) {
+      // eslint-disable-next-line no-console
+      console.log("left corner", absolutePosition.left);
+
+      // eslint-disable-next-line no-console
+      console.log("Event::", event);
+      // eslint-disable-next-line no-console
+      console.log("top corner", absolutePosition.top);
+    }
+  },
+  mounted() {
+    this.draggableValue.boundingElement = this.$refs.bounded;
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+body {
+  background: #eff8ff;
+  height: 100vh;
+  width: 100vh;
+  font-family: system-ui, BlinkMacSystemFont, -apple-system, Segoe UI, Roboto,
+    Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0;
+  padding: 0;
+}
+
+h1 {
+  color: #3d4852;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+a {
+  color: #6cb2eb;
+  font-size: 1.25rem;
+  transition: all 0.1s ease-in;
+  margin-top: 0.5rem;
+  display: block;
+}
+
+a:hover {
+  color: #3490dc;
+}
+
+li,
+p {
+  display: flex;
+  align-items: center;
+  line-height: 1.75;
+  letter-spacing: 0.5px;
+  color: #3d4852;
+  font-size: 1.25rem;
+  cursor: pointer;
+  transition: all 0.1s ease-in;
+}
+
+li:hover {
+  color: #22292f;
+}
+
+li input {
+  margin: 0 0.5rem 0;
+}
+
+#shopping-list > input,
+#shopping-list > select {
+  width: 100%;
+  border-radius: 3px;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+  border: 1px solid #f1f5f8;
+  color: #606f7b;
+  padding: 0.5rem 0.75rem;
+  box-sizing: border-box;
+  font-size: 1rem;
+  letter-spacing: 0.5px;
+  margin: 0.5rem 0;
+}
+
+.add-item-form,
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.add-item-form input {
+  width: 70%;
+  border-radius: 3px;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+  border: 1px solid #f1f5f8;
+  color: #606f7b;
+  padding: 0.5rem 0.75rem;
+  box-sizing: border-box;
+  font-size: 1rem;
+  letter-spacing: 0.5px;
+  margin: 0.5rem 0;
+}
+
+.btn {
+  border: none;
+  border-radius: 3px;
+  margin: auto 0;
+  padding: 0.5rem 0.75rem;
+  flex-shrink: 0;
+  cursor: pointer;
+  font-size: 0.9rem;
+  letter-spacing: 0.5px;
+  transition: all 0.1s ease-in;
+}
+
+.btn[disabled] {
+  background: #8795a1;
+}
+
+.btn[disabled]:hover {
+  background: #606f7b;
+}
+
+.btn-primary {
+  background: #6cb2eb;
+  color: #fff;
+}
+
+.btn-primary:hover {
+  background: #3490dc;
+}
+
+.btn-cancel {
+  background: #ef5753;
+  color: #fff;
+}
+
+.btn-cancel:hover {
+  background: #e3342f;
+  color: #fff;
+}
+
+.strikeout {
+  text-decoration: line-through;
+  color: #b8c2cc;
+}
+
+.strikeout:hover {
+  color: #8795a1;
+}
+
+.priority {
+  color: #de751f;
+}
+</style>
